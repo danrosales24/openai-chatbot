f4cffd41560be552b64c4ba6643f2f431f5e2514
Daniel Rosales <drosales9135@gmail.com>
2023-05-14 01:50:31 -0400

    Added textinput

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import './normalize.css';
 function App() {
   return (
     <div className="App">
-      <aside className='sidemenu'>
-        <h1>Aside</h1>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
       </aside>
       <section className="chatbox">
-
+        <div className="chat-input-holder">
+          <textarea rows="1"className="chat-input-textarea" placeholder="Search a message."></textarea>
+        </div>
       </section>
 
     </div>
